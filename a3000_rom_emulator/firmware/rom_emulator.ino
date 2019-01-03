7530a6a72ab9eb563eb53ccb1b85edcb458737a0
Phillip Pearson <pp@myelin.co.nz>
2019-01-03 12:44:07 -0800

    Successfully reading CFI block from both flash chips

--- a3000_rom_emulator/firmware/rom_emulator.ino
+++ a3000_rom_emulator/firmware/rom_emulator.ino
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <SPI.h>
+
 // For pinPeripheral(), so we can change PINMUX
 #include "wiring_private.h"
 
@@ -45,13 +47,13 @@
 
 // CPLD comms
 
-// PA08 - D35 - cpld_MOSI
+// PA08 / SERCOM2.0 (SERCOM-ALT) - D35 - cpld_MOSI
 #define CPLD_MOSI_PIN 35
-// PA09 - D23 - cpld_SCK
+// PA09 / SERCOM2.1 (SERCOM-ALT) - D23 - cpld_SCK
 #define CPLD_SCK_PIN 23
-// PA10 - D34 - cpld_SS
+// PA10 / SERCOM2.2 (SERCOM-ALT) - D34 - cpld_SS
 #define CPLD_SS_PIN 34
-// PA11 - D22 - cpld_MISO
+// PA11 / SERCOM2.3 (SERCOM-ALT) - D22 - cpld_MISO
 #define CPLD_MISO_PIN 22
 
 // PA14 - D5 - flash_nRESET (pull up)
@@ -66,6 +68,54 @@
 // libxsvf (xsvftool-arduino) entry point
 extern void arduino_play_svf(int tms_pin, int tdi_pin, int tdo_pin, int tck_pin, int trst_pin);
 
+SPIClass cpld_spi(&sercom2, CPLD_MISO_PIN, CPLD_SCK_PIN, CPLD_MOSI_PIN, SPI_PAD_0_SCK_1, SERCOM_RX_PAD_3);
+
+uint8_t spi_transfer(uint8_t b) {
+  uint8_t r = cpld_spi.transfer(b);
+  Serial.print("[");
+  Serial.print(b, HEX);
+  Serial.print(" -> ");
+  Serial.print(r, HEX);
+  Serial.print("]");
+  return r;
+}
+
+void flash_write(uint32_t A, uint32_t D) {
+  // Write a 32-bit word to the flash, leaving allowing_arm_access == 0
+  digitalWrite(CPLD_SS_PIN, LOW);
+  spi_transfer((uint8_t)(0x00 | ((A & 0x3f0000L) >> 16L)));  // allowing_arm_access, rnw, A[21:16]
+  spi_transfer((uint8_t)((A & 0xff00L) >> 8L));  // A[15:8]
+  spi_transfer((uint8_t)(A & 0xffL));  // A[7:0]
+  spi_transfer((uint8_t)((D & 0xff000000L) >> 24L));  // D[31:24]
+  spi_transfer((uint8_t)((D & 0xff0000L) >> 16L));  // D[23:16]
+  spi_transfer((uint8_t)((D & 0xff00L) >> 8L));  // D[15:8]
+  spi_transfer((uint8_t)(D & 0xffL));  // D[7:0]
+  spi_transfer(0);
+  digitalWrite(CPLD_SS_PIN, HIGH);
+}
+
+uint32_t flash_read(uint32_t A) {
+  // Read a 32-bit word from the flash, leaving allowing_arm_access == 0
+  digitalWrite(CPLD_SS_PIN, LOW);
+  spi_transfer((uint8_t)(0x40 | ((A & 0x3f0000) >> 16)));  // allowing_arm_access, rnw, A[21:16]
+  spi_transfer((uint8_t)((A & 0xff00) >> 8));  // A[15:8]
+  spi_transfer((uint8_t)(A & 0xff));  // A[7:0]
+  spi_transfer(0);
+  uint32_t D = ((uint32_t)spi_transfer(0)) << 24;  // D[31:24]
+  D |= ((uint32_t)spi_transfer(0)) << 16;  // D[23:16]
+  D |= ((uint32_t)spi_transfer(0)) << 8;  // D[15:8]
+  D |= ((uint32_t)spi_transfer(0));  // D[7:0]
+  digitalWrite(CPLD_SS_PIN, HIGH);
+  return D;
+}
+
+void flash_unlock() {
+  // Reset allowing_arm_access to 1 in the CPLD
+  digitalWrite(CPLD_SS_PIN, LOW);
+  spi_transfer(0xff);
+  digitalWrite(CPLD_SS_PIN, HIGH);
+}
+
 void setup() {
   // Set pin directions for CPLD JTAG.
   pinMode(TDO_PIN, INPUT);
@@ -77,12 +127,26 @@ void setup() {
 	digitalWrite(TCK_PIN, LOW);
 
   // Set up pullups for flash pins
+  pinMode(FLASH_NRESET_PIN, OUTPUT);
+  digitalWrite(FLASH_NRESET_PIN, LOW);
+  digitalWrite(FLASH_NRESET_PIN, HIGH);
   pinMode(FLASH_NRESET_PIN, INPUT_PULLUP);
   pinMode(FLASH_NREADY_PIN, INPUT_PULLUP);
 
+  // Set up SPI comms on SERCOM2 with CPLD: MOSI=PA08, SCK=PA09, SS=PA10, MISO=PA11
+  // pinMode(CPLD_MOSI_PIN, OUTPUT);
+  // pinMode(CPLD_SCK_PIN, OUTPUT);
+  // pinMode(CPLD_MISO_PIN, INPUT);
+  pinMode(CPLD_SS_PIN, OUTPUT);
+  digitalWrite(CPLD_SS_PIN, HIGH);
+  cpld_spi.begin();
+  pinPeripheral(CPLD_MOSI_PIN, PIO_SERCOM_ALT);
+  pinPeripheral(CPLD_SCK_PIN, PIO_SERCOM_ALT);
+  pinPeripheral(CPLD_MISO_PIN, PIO_SERCOM_ALT);
+  cpld_spi.beginTransaction(SPISettings(24000000L, MSBFIRST, SPI_MODE0));
+
   // Set up USB serial port
   Serial.begin(9600);
-
 }
 
 uint8_t serial_get_uint8() {
@@ -128,6 +192,16 @@ void loop() {
         Serial.println("SEND SVF");
         arduino_play_svf(TMS_PIN, TDI_PIN, TDO_PIN, TCK_PIN, -1);
         Serial.println("SVF DONE");
+        break;
+      }
+      case 'I': {
+        Serial.println("IDENTIFY FLASH");
+        flash_write(0x55, 0x00980098L);
+        Serial.println(" <-- write");
+        for (uint32_t cfi_addr = 0x10; cfi_addr <= 0x50; ++cfi_addr) {
+          Serial.println(flash_read(cfi_addr), HEX);
+        }
+        flash_unlock();
         break;
       }
       case 'Z': {
