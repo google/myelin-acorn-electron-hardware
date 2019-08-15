71b83959caffb6d04fe06a82cf652b45dfe7115f
Phillip Pearson <pp@myelin.co.nz>
2019-08-15 06:44:46 -0700

    Swap port detection around so Arcflash shows up before the USB-serial adapter I'm using to power it

--- simple_cpld_programmer/tools/simple_cpld_programmer.py
+++ simple_cpld_programmer/tools/simple_cpld_programmer.py
@@ -18,7 +18,7 @@ import serial
 
 def guess_port():
     port = None
-    for pattern in "/dev/ttyACM? /dev/ttyUSB? /dev/tty.usbserial* /dev/tty.usbmodem* /dev/tty.wchusbserial*".split():
+    for pattern in "/dev/ttyACM? /dev/ttyUSB? /dev/tty.usbmodem* /dev/tty.usbserial* /dev/tty.wchusbserial*".split():
         matches = glob.glob(pattern)
         if matches:
             return matches[0]
