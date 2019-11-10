79b0fc051662d84dec251ce261c425503a46b18e
Phillip Pearson <philpearson@google.com>
2019-11-10 15:59:17 -0800

    MGC MKII: VHDL now builds; pins not assigned yet and substantial logic still missing.  Uses 31/36 MCs -- may not be able to do fast SPI.

--- mega_games_cartridge_mk2/cpld/mega_games_cartridge_v2.vhdl
+++ mega_games_cartridge_mk2/cpld/mega_games_cartridge_v2.vhdl
@@ -12,12 +12,26 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
 entity MGC is
     port (
+        PHI : in std_logic;
+        CLK16MHz : in std_logic;
         nRESET : in std_logic;
         nPGFC : in std_logic;
+        ERnWMCS : in std_logic;
+        MRnW : in std_logic;
+        nMASDET : in std_logic;
         A : in std_logic_vector(7 downto 0);
-        D : inout std_logic_vector(7 downto 0)
+        D : inout std_logic_vector(7 downto 0);
+
+        spi_nCS : out std_logic := '1';
+        spi_SCK : out std_logic := '0';
+        spi_MOSI : out std_logic := '1';
+        spi_MISO : in std_logic
     );
 end;
 
@@ -33,9 +47,22 @@ architecture rtl of MGC is
 
     signal inhibit_reset : std_logic := '0';
 
-    signal spi_shift_register : std_logic_vector(7 downto 0) := (others => '0');
+    signal spi_shift_register_load : std_logic_vector(7 downto 0) := (others => '0');
+    signal spi_shift_register_16 : std_logic_vector(7 downto 0) := (others => '0');
+    signal spi_start : std_logic := '0';
+    signal spi_start_16 : std_logic := '0';
+    signal last_spi_start_16 : std_logic := '0';
+    signal spi_counter_16 : std_logic_vector(3 downto 0) := (others => '1');
+    signal spi_bit_bang_mode : std_logic := '1';
+    signal spi_bit_bang_MOSI : std_logic := '1';
+    signal spi_bit_bang_SCK : std_logic := '0';
+
+    signal spi_fast_SCK : std_logic := '0';
+    signal spi_fast_MOSI : std_logic := '0';
 
     signal RnW : std_logic;
+
+begin
 
     -- Derive RnW signal
     RnW <= ERnWMCS when nMASDET = '1' else MRnW;
@@ -43,59 +70,72 @@ architecture rtl of MGC is
     -- Handle reads from registers
     D <=
         -- Never drive the bus for CPU writes
-        x"Z" when RnW = '0'
+        "ZZZZZZZZ" when RnW = '0'
         -- Read machine type
         else "0000000" & nMASDET when A = x"FCD2"
         -- Read shift register
-        else spi_shift_register when A = x"FCD4"
+        else spi_shift_register_16 when A = x"FCD4"
         -- Catchall
-        else x"Z";
+        else "ZZZZZZZZ";
 
-    elk_clock_process : process(PHI0)
+    -- Multiplex fast and bitbang SPI
+    spi_SCK <= spi_bit_bang_SCK when spi_bit_bang_mode = '1' else spi_fast_SCK;
+    spi_MOSI <= spi_bit_bang_MOSI when spi_bit_bang_mode = '1' else spi_fast_MOSI;
+
+    -- CPU clock process
+    elk_clock_process : process(PHI)
     begin
-        if falling_edge(PHI0) then
+        if falling_edge(PHI) then
             -- Synchronize external reset
             nRESET_sync <= nRESET;
 
+            -- Reset all one pulse outputs
+            spi_start <= '0';
+
             -- Handle writes to registers
-            if RnW == '0' then
-                if nPGFC = '0' && A == x"D0" then
+            if RnW = '0' then
+                if nPGFC = '0' and A = x"D0" then
                     lower_rom_ram_visible <= '1';
                     upper_rom_ram_visible <= '0';
                 end if;
-                if nPGFC = '0' && A == x"D1" then
+                if nPGFC = '0' and A = x"D1" then
                     upper_rom_ram_visible <= '1';
                     lower_rom_ram_visible <= '0';
                 end if;
-                if nPGFC = '0' && A == x"D3" then
+                if nPGFC = '0' and A = x"D3" then
                     inhibit_reset <= '1';
                 end if;
-                if nPGFC = '0' && A == x"D4" then
-                    spi_shift_register <= D;
-                    -- TODO trigger SPI transfer
+                if nPGFC = '0' and A = x"D4" then
+                    -- Copy D into load register and trigger SPI
+                    spi_shift_register_load <= D;
+                    spi_start <= '1';
+                    spi_bit_bang_mode <= '0';
                 end if;
-                if nPGFC = '0' && A == x"D5" then
-                    -- TODO is this necessary?  writing to FCD4 is probably enough.
-                end if;
-                if nPGFC = '0' && A == x"D8" then
+                if nPGFC = '0' and A = x"D8" then
+                    -- TODO figure out the best bits to use for MMFS
                     spi_nCS <= D(0);
+                    --spi_bit_bang_mode <= D(1);
+                    --spi_bit_bang_MOSI <= D(2);
+                    --spi_bit_bang_SCK <= D(3);
                 end if;
-                if nPGFC = '0' && A == x"D9" then
-                    -- TODO force SPI clock (SPI init?)
+                if nPGFC = '0' and A = x"D9" then
+                    spi_bit_bang_SCK <= D(0);  -- TODO consolidate with FCD8
+                    spi_bit_bang_mode <= '1';
                 end if;
-                if nPGFC = '0' && A == x"DA" then
-                    spi_MOSI <= D(0);  -- TODO is this needed?
+                if nPGFC = '0' and A = x"DA" then
+                    spi_bit_bang_MOSI <= D(0);  -- TODO consolidate with FCD8
+                    spi_bit_bang_mode <= '1';
                 end if;
-                if nPGFC = '0' && A == x"DC" then
+                if nPGFC = '0' and A = x"DC" then
                     lower_rom_locked <= '0';
                 end if;
-                if nPGFC = '0' && A == x"DD" then
+                if nPGFC = '0' and A = x"DD" then
                     lower_rom_locked <= '1';
                 end if;
-                if nPGFC = '0' && A == x"DE" then
+                if nPGFC = '0' and A = x"DE" then
                     upper_rom_locked <= '0';
                 end if;
-                if nPGFC = '0' && A == x"DF" then
+                if nPGFC = '0' and A = x"DF" then
                     upper_rom_locked <= '1';
                 end if;
             end if;
@@ -105,6 +145,38 @@ architecture rtl of MGC is
                 upper_rom_ram_visible <= '1';
                 lower_rom_locked <= '1';
                 upper_rom_locked <= '1';
+            end if;
+        end if;
+    end process;
+
+    -- High speed (16MHz on Electron, 8MHz on Master) clock process
+    fast_clock_process : process(CLK16MHZ)
+    begin
+        if rising_edge(CLK16MHZ) then
+
+            -- Synchronize spi_start
+            last_spi_start_16 <= spi_start_16;
+            spi_start_16 <= spi_start;
+
+            if spi_start_16 = '1' and last_spi_start_16 = '0' then
+                -- start spi transaction
+                spi_counter_16 <= "0000";
+                -- initialize MOSI with the MSB right now, and update it on the falling
+                -- edge of spi_fast_SCK.
+                spi_fast_MOSI <= spi_shift_register_load(7);
+                spi_shift_register_16 <= spi_shift_register_load;
+            end if;
+
+            if spi_counter_16(3) = '0' then
+                -- spi active!
+                if spi_fast_SCK = '0' then
+                    spi_fast_SCK <= '1';
+                    spi_shift_register_16 <= spi_shift_register_16(6 downto 0) & spi_MISO;
+                else
+                    spi_fast_SCK <= '0';
+                    spi_fast_MOSI <= spi_shift_register_16(7);
+                end if;
+                spi_counter_16 <= std_logic_vector(unsigned(spi_counter_16) + 1);
             end if;
         end if;
     end process;
