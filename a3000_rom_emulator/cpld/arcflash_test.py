dcff53edbe9abaa70228dbe93c8581a2575e855d
Phillip Pearson <pp@myelin.nz>
2025-01-21 23:42:44 -0800

    arcflash: Test reading a word from flash

--- a3000_rom_emulator/cpld/arcflash_test.py
+++ a3000_rom_emulator/cpld/arcflash_test.py
@@ -16,11 +16,17 @@
 
 
 import cocotb
+from cocotb.handle import Force, Release
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, FallingEdge, RisingEdge
 
+
 def tick():
     return Timer(1, units="ns")
+
+
+def fmt_byte_array(data):
+    return " ".join("%02X" % c for c in data)
 
 
 async def spi_send_byte(dut, b):
@@ -31,7 +37,16 @@ async def spi_send_byte(dut, b):
         dut.cpld_SCK.value = 1
         await tick()
         dut.cpld_SCK.value = 0
+        # dut._log.info(
+        #     "spi; cpld_MISO=%s enable_bitbang_serial=%s cpld_SS+%s cpld_MISO_TXD=%s cpld_MISO_int=%s",
+        #     dut.cpld_MISO.value,
+        #     dut.enable_bitbang_serial,
+        #     dut.cpld_SS.value,
+        #     dut.cpld_MISO_TXD.value,
+        #     dut.cpld_MISO_int.value,
+        # )
         r = (r << 1) | dut.cpld_MISO.value
+        b = (b << 1) & 0xFF
     return r
 
 
@@ -44,18 +59,67 @@ async def spi_send(dut, spi_bytes):
     expected_bits = 0
     for b in spi_bytes:
         ret.append(await spi_send_byte(dut, b))
-        expected_bits += 8
-        assert dut.spi_bit_count == expected_bits
+        expected_bits = (expected_bits + 8) % 64
+        assert (
+            dut.spi_bit_count == expected_bits
+        ), "spi_bit_count is %d but expected %d" % (dut.spi_bit_count, expected_bits)
     dut.cpld_SS.value = 1
     await tick()
 
+    dut._log.info(
+        "spi transaction: sent %s received %s",
+        fmt_byte_array(spi_bytes),
+        fmt_byte_array(ret),
+    )
     return ret
+
+
+async def spi_flash_write(dut, addr, data):
+    resp = await spi_send(
+        dut,
+        [
+            # 0, RnW, 22 address bits, 40 zeroes.
+            (addr & 0x3F0000) >> 16,
+            (addr & 0xFF00) >> 8,
+            addr & 0xFF,
+            (data & 0xFF000000) >> 24,
+            (data & 0xFF0000) >> 16,
+            (data & 0xFF00) >> 8,
+            data & 0xFF,
+            0,
+        ],
+    )
+
+
+async def spi_flash_read(dut, addr):
+    resp = await spi_send(
+        dut,
+        [
+            # 0, RnW, 22 address bits, 40 zeroes.
+            ((addr & 0x3F0000) >> 16) | 0x40,
+            (addr & 0xFF00) >> 8,
+            addr & 0xFF,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ],
+    )
+    return (resp[-4] << 24) | (resp[-3] << 16) | (resp[-2] << 8) | resp[-1]
 
 
 def init(dut):
     dut.rom_5V.value = 1
     dut.rom_nCS.value = 0
+    dut.flash0_DQ = Release()
+    dut.flash1_DQ = Release()
     dut.rom_nOE.value = 0
+    dut.cpld_MISO_int.value = 1
+    dut.cpld_MISO_TXD.value = 0
+    dut.spi_A = 0
+    dut.spi_D = 0
+    dut.spi_bit_count.value = 0
 
 
 @cocotb.test()
@@ -65,7 +129,6 @@ async def disable_arm_access(dut):
     init(dut)
 
     spi_resp = await spi_send(dut, [0x7F, 0xFF, 0xFF, 0xFF])
-    dut._log.info("spi response: %s", spi_resp)
     dut._log.info("allowing_arm_access: %s", dut.allowing_arm_access.value)
     dut._log.info(
         "flash nCE %s nOE %s nWE %s",
@@ -86,7 +149,6 @@ async def enable_arm_access(dut):
     init(dut)
 
     spi_resp = await spi_send(dut, [0xFF, 0xFF, 0xFF, 0xFF])
-    dut._log.info("spi response: %s", spi_resp)
     dut._log.info("allowing_arm_access: %s", dut.allowing_arm_access.value)
     dut._log.info(
         "flash nCE %s nOE %s nWE %s",
@@ -98,3 +160,32 @@ async def enable_arm_access(dut):
     assert dut.flash_nCE.value == 0
     assert dut.flash_nOE.value == 0
     assert dut.flash_nWE.value == 1
+
+
+@cocotb.test()
+async def read_from_flash(dut):
+    """Read a word from flash."""
+
+    init(dut)
+
+    # Read command: 0, 0, 22 address bits, 40 zeroes.
+    # Data is in the last four bytes of the SPI response.
+    test_word = 0x12345678
+    dut.flash1_DQ.value = Force((test_word >> 16) & 0xFFFF)
+    dut.flash0_DQ.value = Force(test_word & 0xFFFF)
+    read_resp = await spi_flash_read(dut, 0x3BCDEF)
+    dut._log.info(
+        "Word read: %08X; flash_DQ %X %X",
+        read_resp,
+        dut.flash1_DQ.value,
+        dut.flash0_DQ.value,
+    )
+    assert (
+        dut.allowing_arm_access.value == 0
+    ), "Flash read should leave ARM access disabled"
+    assert (
+        read_resp == test_word
+    ), "Flash read returned incorrect data: %08X instead of %08X" % (
+        read_resp,
+        test_word,
+    )
